b6e66727f747bf3d082501e61075707ba39553ae
Gustavo Cotta <gmcotta34@gmail.com>
2020-02-13 16:36:48 -0200

    Persist city on Local Storage

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
-const city = 'Guarulhos';
-const weather = new Weather(city);
+const storage = new Storage();
+const weatherCity = storage.getLocationData();
+
+const weather = new Weather(weatherCity);
 const ui = new UI();
 
 document.addEventListener('DOMContentLoaded', getWeather);
 
-// weather.changeLocation('São Paulo');
+document.getElementById('w-change-btn').addEventListener('click', evt => {
+  evt.preventDefault();
+  
+  const city = document.getElementById('city').value;
+  weather.changeLocation(city);
+  storage.setLocationData(city);
+  getWeather();
+
+  $('#locModal').modal('hide');
+});
+
 
 function getWeather() {
   weather.getWeather()
-    .then(data => ui.paint(data))
+    .then(data => {
+      ui.paint(data);
+    })
     .catch(err => console.log(err));
 }
 
